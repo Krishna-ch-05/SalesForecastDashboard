7bace7ab80e4e8477764097095eede79d25aa727
Krishna Chahar <krishnachahar945@gmail.com>
2026-07-10 14:46:58 +0530

    Specify date format for Order Date conversion

--- pages/1_Sales_Overview.py
+++ pages/1_Sales_Overview.py
@@ -10,7 +10,10 @@ st.title("📈 Sales Overview Dashboard")
 @st.cache_data
 def load_data():
     df = pd.read_csv("train.csv")
-    df["Order Date"] = pd.to_datetime(df["Order Date"])
+    df["Order Date"] = pd.to_datetime(
+    df["Order Date"],
+    format="%m/%d/%Y"
+)
     df["Year"] = df["Order Date"].dt.year
     df["Month"] = df["Order Date"].dt.month_name()
     return df
